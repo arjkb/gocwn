b559da73716501fed4e01e03a9bc6fc3e5579799
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-07-30 08:06:16 +0530

    call API and print all results

--- gocwn.go
+++ gocwn.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	// "net/http"
+	"log"
+	"net/http"
 	"net/url"
 	"strconv"
 	"time"
@@ -19,10 +21,11 @@ type Center struct {
 
 type Session struct {
 	Date          string
+	Vaccine       string
 	MinimumAge    int  `json:"min_age_limit"`
 	AllowAllAge   bool `json:"allow_all_age"`
-	CapacityDose1 int
-	CapacityDose2 int
+	CapacityDose1 int  `json:"available_capacity_dose1"`
+	CapacityDose2 int  `json:"available_capacity_dose2"`
 }
 
 func GenerateApiUrl(districtId int, date time.Time) string {
@@ -36,6 +39,34 @@ func GenerateApiUrl(districtId int, date time.Time) string {
 	return BASE_URL_CALENDAR_BY_DISTRICT + "?" + v.Encode()
 }
 
+func GetResults(district_id int, date time.Time) (*ApiResult, error) {
+	resp, err := http.Get(GenerateApiUrl(district_id, date))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Query failed: %s", resp.Status)
+	}
+
+	var result ApiResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
+
 func main() {
-	fmt.Println(GenerateApiUrl(307, time.Now()))
+	results, err := GetResults(307, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, center := range results.Centers {
+		for _, session := range center.Sessions {
+			fmt.Printf("%-40s %s %-12s %d %4d %4d\n", center.Name, session.Date, session.Vaccine, session.MinimumAge, session.CapacityDose1, session.CapacityDose2)
+		}
+	}
 }
