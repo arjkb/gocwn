52bc4e689e65b844b4282b93b45909a47084aaac
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-07-30 07:42:25 +0530

    declare structs that would hold the api results

--- gocwn.go
+++ gocwn.go
@@ -2,7 +2,30 @@ package main
 
 import (
 	"fmt"
+	// "net/http"
+	"net/url"
+	"strconv"
+	"time"
 )
+
+const BASE_URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict"
+
+type ApiResult struct {
+	Centers []*Center
+}
+
+type Center struct {
+	Name     string
+	Sessions []*Session
+}
+
+type Session struct {
+	Date          string
+	MinimumAge    int  `json:"min_age_limit"`
+	AllowAllAge   bool `json:"allow_all_age"`
+	CapacityDose1 int
+	CapacityDose2 int
+}
 
 func main() {
 	fmt.Println("hello")
