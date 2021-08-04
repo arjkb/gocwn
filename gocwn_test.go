8e87af337569bf66943f90a7d6e5cb62e7b77378
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2021-08-04 21:46:21 +0530

    create (currently failing) testcases to test GetAgeBracket() function

--- gocwn_test.go
+++ gocwn_test.go
@@ -34,3 +34,66 @@ func TestGenerateApiUrl01Mar2020(t *testing.T) {
 		t.Fatalf(`Expected %s, got %s`, expectedUrl, generatedUrl)
 	}
 }
+
+// Test age bracket with the following params:
+// MinimumAge=10, MaximumAge=20, AllowAllAge=false
+func TestAgeBracketMin10Max20AllowAllFalse(t *testing.T) {
+	validSession := Session{
+		Date:          "1-2-2003",
+		Vaccine:       "COVISHIELD",
+		MinimumAge:    10,
+		MaximumAge:    20,
+		AllowAllAge:   false,
+		CapacityDose1: 100,
+		CapacityDose2: 200,
+	}
+
+	expectedOutput := "10-20"
+	actualOutput := GetAgeBracket(validSession)
+
+	if actualOutput != expectedOutput {
+		t.Fatalf(`Expected %q, got %q`, actualOutput, expectedOutput)
+	}
+}
+
+// Test age bracket with the following params:
+// MinimumAge=10, MaximumAge=20, AllowAllAge=true
+func TestAgeBracketMin10Max20AllowAllTrue(t *testing.T) {
+	validSession := Session{
+		Date:          "1-2-2003",
+		Vaccine:       "COVISHIELD",
+		MinimumAge:    10,
+		MaximumAge:    20,
+		AllowAllAge:   false,
+		CapacityDose1: 100,
+		CapacityDose2: 200,
+	}
+
+	expectedOutput := "10+"
+	actualOutput := GetAgeBracket(validSession)
+
+	if actualOutput != expectedOutput {
+		t.Fatalf(`Expected %q, got %q`, actualOutput, expectedOutput)
+	}
+}
+
+// Test age bracket with the following params:
+// MinimumAge=10, MaximumAge=0 (zero-value), AllowAllAge=false
+func TestAgeBracketMin10Max0AllowAllTrue(t *testing.T) {
+	validSession := Session{
+		Date:          "1-2-2003",
+		Vaccine:       "COVISHIELD",
+		MinimumAge:    10,
+		MaximumAge:    20,
+		AllowAllAge:   false,
+		CapacityDose1: 100,
+		CapacityDose2: 200,
+	}
+
+	expectedOutput := "10+"
+	actualOutput := GetAgeBracket(validSession)
+
+	if actualOutput != expectedOutput {
+		t.Fatalf(`Expected %q, got %q`, actualOutput, expectedOutput)
+	}
+}
